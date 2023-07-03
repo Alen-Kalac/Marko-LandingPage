0e8c4d2f4907c5d00234e0ca9b4b1d4a4bbf3da5
Alen-Kalac <alengames@live.com>
2023-07-03 11:26:37 +0200

    added animations on all needed elements

--- app.js
+++ app.js
@@ -15,3 +15,20 @@ window.addEventListener('scroll', function() {
       }
     });
   });
+  window.addEventListener('DOMContentLoaded', function() {
+    var tags = document.querySelectorAll('.animated-onload');
+  
+    tags.forEach(function(tag) {
+      var tagTop = tag.getBoundingClientRect().top;
+      var tagBottom = tag.getBoundingClientRect().bottom;
+      var screenHeight = window.innerHeight;
+  
+      if (tagTop < screenHeight && tagBottom >= 0) {
+        tag.style.opacity = 1;
+        tag.style.transform = 'translateY(0)';
+      } else {
+        tag.style.opacity = 0;
+        tag.style.transform = 'translateY(50px)';
+      }
+    });
+  });
